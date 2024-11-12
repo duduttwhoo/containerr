767fed0ec2ca949151505fc655e1762a1940fe55
duduttt <duduttxyy@gmail.com>
2024-11-12 14:37:09 +0700

    MYTRIP

--- main.dart
+++ main.dart
@@ -1,66 +1,195 @@
+import 'package:belajar_widget/styles.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
+    const String appTitle = 'MY TRIP MY ADVENTURE';
     return MaterialApp(
+        title: appTitle,
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('Container Widget Example'),
-        centerTitle: true,
-      ),
-      body:Center(
-        child: Container(
-          width: 230,
-          height: 300,
-          margin: const EdgeInsets.all(16.0),
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-              color: const Color.fromRGBO(0, 195, 255, 1),
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  spreadRadius: 2,
-                  blurRadius: 8,
-                  offset: const Offset(0, 4),
-                ),
-              ],
-            ), 
+            appBar: AppBar(
+              title: const Text(appTitle),
+              centerTitle: true,
+            ),
+            body: const SingleChildScrollView(
+              child: Column(
+                children: [
+                  ImageSection(
+                    image: 'assets/images/banda.jpg',
+                  ),
+                  TitleSection(
+                    name: 'Banda Neira',
+                    location: 'Banda Islands in Maluku province, Indonesia',
+                  ),
+                  ButtonSection(),
+                  TextSection(
+                    description:
+                        'Banda Neira or Banda Naira is one of ten volcanic islands in the Banda Archipelago in the Maluku province,'
+                        'in eastern Indonesia. The Banda Islands, probably better known as the fabled Spice Islands of ancient times, are covered with towering, aromatic, tropical-evergreens. '
+                        'The Myristica Trees, indigenous to the Banda Islands, are the source of two rare and lucrative spices: nutmeg and mace, once found exclusively on the Banda Islands.'
+                        'A sparkling, sapphire sea surrounds these already incredible islands, brimming with exquisite corals and abundant ocean life, '
+                        'making the Banda Islands not only a historically significant destination, but also an internationally recognized diving spot.',
+              ),
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
-                  'title flutter stuff',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-                  ),
-                  const SizedBox(height: 8),
-                  const Divider(color: Colors.black),
-                  const Text(
-                    'description: this is a card-like layout using a container widget.it has padding,margin,and a box shadow.',
-                    style: TextStyle(
-                      color: Colors.white,
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    height: 40,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('visit to read more')))
+                ),
+                Text(
+                  location,
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
               ],
             ),
           ),
-        ),
+          Icon(
+            Icons.star,
+            color: const Color.fromARGB(255, 255, 221, 0),
+          ),
+          const Text('4,8'),
+        ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(
+            color: const Color.fromRGBO(0, 128, 255, 1),
+            icon: Icons.call,
+            label: 'CALL',
+          ),
+          ButtonWithText(
+            color: const Color.fromRGBO(0, 128, 255, 1),
+            icon: Icons.near_me,
+            label: 'ROUTE',
+          ),
+          ButtonWithText(
+            color: const Color.fromRGBO(0, 128, 255, 1),
+            icon: Icons.share,
+            label: 'SHARE',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({
+    super.key,
+    required this.description,
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
+      ),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image,
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
     );
   }
 }
